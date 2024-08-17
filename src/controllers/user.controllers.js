46975d5717f06ece7abd72cdf983c5c9e000f2db
rahul-7416 <rj.cu74@gmail.com>
2024-08-17 17:40:44 +0530

    added some more cookie options in other routes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -144,6 +144,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
         const options = {
             httpOnly: true,
             secure: true,
+            sameSite: 'None'
         }
     
         const { accessToken, refreshToken: newRefreshToken } = await generateAccessTokenAndRefreshToken(user._id);
